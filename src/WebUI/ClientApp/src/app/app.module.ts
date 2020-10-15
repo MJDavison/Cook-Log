8ad72bbecdb07da5bb07027565eaf538044d296a
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-10-15 22:31:48 +0100

    Installed Bootstrap

--- src/WebUI/ClientApp/src/app/app.module.ts
+++ src/WebUI/ClientApp/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { TopComponent } from './navigation/top/top.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
